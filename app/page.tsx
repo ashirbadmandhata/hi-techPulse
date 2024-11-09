298ee25f2b6b263dbca90bee02824bd2c88e8245
Ashirbad Mandhata <96368983+ashirbadmandhata@users.noreply.github.com>
2024-11-09 11:16:44 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const Home = ({ searchParams }: SearchParamProps) => {
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 HI-Tech Pulse(Ashirbad Mandhata)
+              © 2024 HI-Tech Pulse(Anbikeswari And Group)
             </p>
             <Link href='/?admin=true' className="text-green-500">
               Admin
